2179f0ae780761ade08b1aa5fce0ebd1c0eabe7d
SKILL573 <a9101021249@gmail.com>
2017-12-13 19:07:05 +0800

    no message

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -2,7 +2,6 @@
 <html lang="en">
 
   <head>
-
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
@@ -14,7 +13,6 @@
   </head>
 
   <body>
-
     <!-- Navigation -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
       <div class="container">
@@ -46,7 +44,6 @@
     <!-- Page Content -->
     <div class="container">
       <div class="row">
-
         <div class="col-lg-3">
           /n
           <h1 class="my-4">Shop Name</h1>
@@ -55,7 +52,6 @@
             <a href="#" class="list-group-item">Category 2</a>
             <a href="#" class="list-group-item">Category 3</a>
           </div>
-
         </div>
         <!-- /.col-lg-3 -->
 
