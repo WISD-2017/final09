decaf75904253cb53855b67341c4c458c0e19fce
3A432053許漢昌 <s3a432053@student.ncut.edu.tw>
2017-12-15 01:13:37 +0800

    修改關聯的資料表名稱，以及加上fillable

--- app/Books.php
+++ app/Books.php
@@ -6,9 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Books extends Model
 {
-    protected $table="{product}";
+
+    protected $fillable=[
+        'book_id',
+        'book_name',
+        'inventory',
+        'ISBN',
+        'category_id',
+        'text',
+        'price',
+    ];
+
+    protected $table="books";
 public function Bookcategury(){
-    return $this->hasOne(Bookcategury::class);
+    return $this->hasOne(Bookcategory::class);
 }
 public function Order(){
     return $this->belongsTo(Order::class);
