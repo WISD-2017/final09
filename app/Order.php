88a1ff6d29b410f5723b6ae8f2f8c0faf3e17fae
SKILL573 <a9101021249@gmail.com>
2017-12-14 16:31:00 +0800

    建立MODEL

--- app/Order.php
+++ app/Order.php
@@ -9,10 +9,5 @@ class Order extends Model
     protected $table='{order}';
     protected $fillable=['order_id','book_id','quantity'];
     public $timestamps=true;
-    public function author(){
-        return $this->belongsTo(Order::class,'book_id','order_id');
-    }
-    public function postType(){
-        return $this->hasOne(PostTypy::class,'id','Order_id');
-    }
+    
 }
