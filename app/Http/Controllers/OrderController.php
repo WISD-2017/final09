fbb417f44963821e38edc4533759c27b9ad02761
SKILL573 <a9101021249@gmail.com>
2017-12-14 16:19:09 +0800

    新增訂單查詢方法

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -1,12 +1,16 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
-
+use Illuminate\View\View;
+use APP\Order as PostRequest;
 class OrderController extends Controller
 {
   public function index(){
       return view('admin.index');
+  }
+  public function ordershow(PostRequest $request ){
+    $order=new PostEloquent::findOrFail($order_id);
+    return View::make('admin.orderlist',['order'=>$order]);
   }
 }
