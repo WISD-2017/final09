dde52641ba0ee2cc8839077a7c7cd6d337cf9ba3
SKILL573 <a9101021249@gmail.com>
2017-12-14 17:15:15 +0800

    撰寫SEEDER內容

--- database/seeds/Order.php
+++ database/seeds/Order.php
@@ -11,6 +11,16 @@ class Order extends Seeder
      */
     public function run()
     {
-        //
+        foreach (range(1,20)as $number){
+            Order::creat([
+                'category_id'=>'category_id'.$number,
+                'book_id'=>'book_id'.$number,
+                'discount'=>rand(0,0.5),
+                'quantity'=>rand(1,99) ,
+                'orderdate'=>Carbon::now()->addDays($number),
+                'principal'=>rand(5000,10000) ,
+                'member_id'=>'member_id'.$number
+            ]);
+        }
     }
 }
