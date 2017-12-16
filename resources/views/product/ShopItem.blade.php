b4e0c4f3815daaa42cec52055eee53b323218543
3A432053許漢昌 <s3a432053@student.ncut.edu.tw>
2017-12-16 23:18:07 +0800

    將資料庫抓取的資料呈現在view上

--- resources/views/product/ShopItem.blade.php
+++ resources/views/product/ShopItem.blade.php
@@ -69,7 +69,9 @@
             <img class="card-img-top img-fluid" src="http://placehold.it/900x400" alt="">
             <div class="card-body">
               <h3 class="card-title">Product Name</h3>
-              <h4>$24.99</h4>
+              @foreach ($books as $book)
+                <p>價格：{{ $book->price }}$</p>
+              @endforeach
               <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente dicta fugit fugiat hic aliquam itaque facere, soluta. Totam id dolores, sint aperiam sequi pariatur praesentium animi perspiciatis molestias iure, ducimus!</p>
               <span class="text-warning">&#9733; &#9733; &#9733; &#9733; &#9734;</span>
               4.0 stars
