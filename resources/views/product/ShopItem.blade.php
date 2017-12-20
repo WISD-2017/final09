70805df82a3fa367081161fe912bbf260a7c8d94
3A432053許漢昌 <s3a432053@student.ncut.edu.tw>
2017-12-21 00:13:42 +0800

    修改呈現的資料

--- resources/views/product/ShopItem.blade.php
+++ resources/views/product/ShopItem.blade.php
@@ -70,7 +70,7 @@
             <div class="card-body">
               <h3 class="card-title">Product Name</h3>
               @foreach ($books as $book)
-                <p>價格：{{ $book->price }}$</p>
+                <p>價格：{!! $book->first()->price !!}$</p>
               @endforeach
               <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente dicta fugit fugiat hic aliquam itaque facere, soluta. Totam id dolores, sint aperiam sequi pariatur praesentium animi perspiciatis molestias iure, ducimus!</p>
               <span class="text-warning">&#9733; &#9733; &#9733; &#9733; &#9734;</span>
