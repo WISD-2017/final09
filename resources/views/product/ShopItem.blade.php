31994483279c02f082a0f1547f4e7cd64ad90bc0
3A432053許漢昌 <s3a432053@student.ncut.edu.tw>
2017-12-22 01:05:09 +0800

    呈現書籍價格以及名稱

--- resources/views/product/ShopItem.blade.php
+++ resources/views/product/ShopItem.blade.php
@@ -54,7 +54,7 @@
       <div class="row">
 
         <div class="col-lg-3">
-          <h1 class="my-4">Shop Name</h1>
+          <h1 class="my-4">書籍資訊</h1>
           <div class="list-group">
             <a href="#" class="list-group-item active">Category 1</a>
             <a href="#" class="list-group-item">Category 2</a>
@@ -68,9 +68,9 @@
           <div class="card mt-4">
             <img class="card-img-top img-fluid" src="http://placehold.it/900x400" alt="">
             <div class="card-body">
-              <h3 class="card-title">Product Name</h3>
               @foreach ($books as $book)
-                <p>價格：{!! $book->first()->price !!}$</p>
+                <p>{{$book->book_name}}</p>
+                <p>價格：{{$book->price}}$</p>
               @endforeach
               <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente dicta fugit fugiat hic aliquam itaque facere, soluta. Totam id dolores, sint aperiam sequi pariatur praesentium animi perspiciatis molestias iure, ducimus!</p>
               <span class="text-warning">&#9733; &#9733; &#9733; &#9733; &#9734;</span>
