5e0dc21f9ec33864e96e4d5e2d0c7f1ddfbe4d2e
3A432053許漢昌 <s3a432053@student.ncut.edu.tw>
2017-12-22 01:06:05 +0800

    新增建立資料的程式碼以及讓route辨別book_id

--- routes/web.php
+++ routes/web.php
@@ -11,31 +11,50 @@
 |
 */
 
-Route::get('/',['as'=>'firstpage', function () {
-    /*
+Route::get('/', ['as' => 'firstpage', function () {
+
     \App\Books::create([
-        'book_id'=>'002',
-        'book_name'=>'呂世浩細說史記：入門篇',
-        'inventory'=>'30',
-        'ISBN'=>'9789571371436',
-        'category_id'=>'002',
-        'text'=>'新書',
-        'price'=>'237',
+        /*
+                'book_id'=>'001',
+                'book_name'=>'教育是一場騙局',
+                'inventory'=>'20',
+                'ISBN'=>'9789571367460',
+                'category_id'=>'001',
+                'text''新書',
+                'price'=>'378',
+
+        'book_id' => '002',
+        'book_name' => '呂世浩細說史記：入門篇',
+        'inventory' => '30',
+        'ISBN' => '9789571371436',
+        'category_id' => '002',
+        'text' => '新書',
+        'price' => '237',
+*/
     ]);
-    */
-    return view('welcome');}]);
+
+    \App\Bookcategory::create([
+                'category_id'=>'001',
+                'category_name'=>'小說',
+        /*
+        'category_id' => '002',
+        'category_name' => '文學',
+*/
+    ]);
+    return view('welcome');
+}]);
 //
 // 後台
-Route::group(['prefix' => 'admin'], function() {
+Route::group(['prefix' => 'admin'], function () {
     Route::get('/index', ['as' => 'admin.index', 'uses' => 'OrderController@index']);
-    Route::get('/memberlist',['as'=>'admin.member','uses'=>'OrderController@member']);
-    Route::get('/orderlist',['as'=>'admin.order','uses'=>'OrderController@order']);
-    Route::get('/productlist',['as'=>'admin.product','uses'=>'OrderController@product']);
+    Route::get('/memberlist', ['as' => 'admin.member', 'uses' => 'OrderController@member']);
+    Route::get('/orderlist', ['as' => 'admin.order', 'uses' => 'OrderController@order']);
+    Route::get('/productlist', ['as' => 'admin.product', 'uses' => 'OrderController@product']);
 });
 
-Route::group(['prefix'=>'product'],function (){
+Route::group(['prefix' => 'product'], function () {
     Route::get('/shoplist', ['as' => 'product.shoplist', 'uses' => 'ProductController@ShowProductList']);
-    Route::get('/shopitem',['as' => 'product.shopitem', 'uses' => 'ProductController@ShowProductDetail']);
+    Route::get('/shopitem/{bookid}', ['as' => 'product.shopitem', 'uses' => 'ProductController@ShowProductDetail']);
 });
 
 
