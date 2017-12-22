bf76071ce52834f70ea34b50e6b1e5502f23d839
3A432053許漢昌 <s3a432053@student.ncut.edu.tw>
2017-12-23 01:07:31 +0800

    model錯誤修正

--- app/Bookcategory.php
+++ app/Bookcategory.php
@@ -6,7 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Bookcategory extends Model
 {
-    protected $table="{bookcategory}";
+    protected $fillable=[
+        'category_id',
+        'category_name',
+    ];
+
+    protected $table="bookcategory";
     public function Books() {
         return $this->belongsTo(Books::class);
     }
