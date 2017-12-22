3919e2e89a866747fd19925924a30db13d01ac0d
3A432053許漢昌 <s3a432053@student.ncut.edu.tw>
2017-12-23 01:07:55 +0800

    新增資料的錯誤修正

--- routes/web.php
+++ routes/web.php
@@ -12,30 +12,28 @@
 */
 
 Route::get('/', ['as' => 'firstpage', function () {
+    /*
+        \App\Books::create([
+                    'book_id'=>'001',
+                    'book_name'=>'教育是一場騙局',
+                    'inventory'=>'20',
+                    'ISBN'=>'9789571367460',
+                    'category_id'=>'001',
+                    'text''新書',
+                    'price'=>'378',
 
-    \App\Books::create([
-        /*
-                'book_id'=>'001',
-                'book_name'=>'教育是一場騙局',
-                'inventory'=>'20',
-                'ISBN'=>'9789571367460',
-                'category_id'=>'001',
-                'text''新書',
-                'price'=>'378',
-
-        'book_id' => '002',
-        'book_name' => '呂世浩細說史記：入門篇',
-        'inventory' => '30',
-        'ISBN' => '9789571371436',
-        'category_id' => '002',
-        'text' => '新書',
-        'price' => '237',
-*/
-    ]);
-
+            'book_id' => '002',
+            'book_name' => '呂世浩細說史記：入門篇',
+            'inventory' => '30',
+            'ISBN' => '9789571371436',
+            'category_id' => '002',
+            'text' => '新書',
+            'price' => '237',
+        ]);
+        */
     \App\Bookcategory::create([
-                'category_id'=>'001',
-                'category_name'=>'小說',
+        'category_id'=>'001',
+        'category_name'=>'小說',
         /*
         'category_id' => '002',
         'category_name' => '文學',
