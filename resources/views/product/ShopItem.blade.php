e71c77e08a3d1bcb1f2fa5f29d7aff261bc760e6
3A432053許漢昌 <s3a432053@student.ncut.edu.tw>
2017-12-26 00:44:20 +0800

    塞入圖片

--- resources/views/product/ShopItem.blade.php
+++ resources/views/product/ShopItem.blade.php
@@ -59,52 +59,56 @@
             <div class="list-group">
                 <h3 class="my-4">書籍類別</h3>
                 @foreach($categorys as $category)
-                    <a href="{{route('product.shoplist',$category->id)}}" class="list-group-item">{{$category->category_name}}</a>
+                    <a href="{{route('product.shoplist',$category->id)}}"
+                       class="list-group-item">{{$category->category_name}}</a>
                 @endforeach
             </div>
         </div>
         <!-- /.col-lg-3 -->
 
         <div class="col-lg-9">
-
-            <div class="card mt-4">
-                <img class="card-img-top img-fluid" src="http://placehold.it/900x400" alt="">
-                <div class="card-body">
-                    @foreach ($books as $book)
-                        <p>書名{{$book->book_name}}</p>
-                        <p>價格：${{$book->price}}</p>
-                    @endforeach
-                    書籍介紹：
-                    @foreach($books as $book)
-                    <p class="card-text">{{$book->text}}</p>
+            @foreach($books as $book)
+                <div class="card mt-4">
+                    <img class="card-img-top img-fluid" src="{{url('../storage/app/',$book->path)}}"width="300px" height="200px" style="border: 5px solid; border-radius: 12px">
+                    <div class="card-body">
                         @endforeach
+                        @foreach ($books as $book)
+                            <p>書名{{$book->book_name}}</p>
+                            <p>價格：${{$book->price}}</p>
+                        @endforeach
+                        書籍介紹：
+                        @foreach($books as $book)
+                            <p class="card-text">{{$book->text}}</p>
+                        @endforeach
+                    </div>
                 </div>
-            </div>
-            <!-- /.card -->
+                <!-- /.card -->
 
-            <div class="card card-outline-secondary my-4">
-                <div class="card-header">
-                    書籍資訊
+                <div class="card card-outline-secondary my-4">
+                    <div class="card-header">
+                        書籍資訊
+                    </div>
+                    <div class="card-body">
+                        @foreach($books as $book)
+                            <p class="card-text">ISBN:{{$book->ISBN}}</p>
+                        @endforeach
+                        <hr>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis et enim aperiam inventore,
+                            similique necessitatibus neque non! Doloribus, modi sapiente laboriosam aperiam fugiat
+                            laborum.
+                            Sequi mollitia, necessitatibus quae sint natus.</p>
+                        <small class="text-muted">Posted by Anonymous on 3/1/17</small>
+                        <hr>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis et enim aperiam inventore,
+                            similique necessitatibus neque non! Doloribus, modi sapiente laboriosam aperiam fugiat
+                            laborum.
+                            Sequi mollitia, necessitatibus quae sint natus.</p>
+                        <small class="text-muted">Posted by Anonymous on 3/1/17</small>
+                        <hr>
+                        <a href="#" class="btn btn-success">Leave a Review</a>
+                    </div>
                 </div>
-                <div class="card-body">
-                    @foreach($books as $book)
-                        <p class="card-text">ISBN{{$book->ISBN}}</p>
-                        @endforeach：
-                    <hr>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis et enim aperiam inventore,
-                        similique necessitatibus neque non! Doloribus, modi sapiente laboriosam aperiam fugiat laborum.
-                        Sequi mollitia, necessitatibus quae sint natus.</p>
-                    <small class="text-muted">Posted by Anonymous on 3/1/17</small>
-                    <hr>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis et enim aperiam inventore,
-                        similique necessitatibus neque non! Doloribus, modi sapiente laboriosam aperiam fugiat laborum.
-                        Sequi mollitia, necessitatibus quae sint natus.</p>
-                    <small class="text-muted">Posted by Anonymous on 3/1/17</small>
-                    <hr>
-                    <a href="#" class="btn btn-success">Leave a Review</a>
-                </div>
-            </div>
-            <!-- /.card -->
+                <!-- /.card -->
 
         </div>
         <!-- /.col-lg-9 -->
