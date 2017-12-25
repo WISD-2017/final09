097d86ca27a5a74bdd4ff623fe00fc7a74c6868d
3A432053許漢昌 <s3a432053@student.ncut.edu.tw>
2017-12-25 21:30:10 +0800

    修改Controller，新增showrroductindex方法來抓取首頁資料 依照類別id來抓取書籍資料以及抓取所有類別資料

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,8 +7,18 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    public function ShowProductList(){
-    return view('product.index');
+    public function ShowProductList($categoryid){
+        $books=books::all()->where('category_id',$categoryid);
+        $categorys=bookcategory::all();
+        return view('product.index',compact('categorys','books'));
+    }
+
+
+    public function ShowProductIndex(){
+        $books=books::all();
+        $categorys=bookcategory::all();
+        return view('product.index',compact('categorys','books'));
+
     }
 
     public function ShowProductDetail($bookid){
