661ca2551690359380f9dcc84e81a552289e3016
3A432053許漢昌 <s3a432053@student.ncut.edu.tw>
2017-12-28 21:13:20 +0800

    將購物車資料抓出來

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -15,12 +15,11 @@ class ProductController extends Controller
         return view('product.index',compact('categorys','books'));
     }
 
-
-    public function ShowProductIndex(){
-        $books=books::all();
-        $categorys=bookcategory::all();
-        return view('product.index',compact('categorys','books'));
-
+    public function ShowProductIndex()
+    {
+        $books = books::all();
+        $categorys = bookcategory::all();
+        return view('product.index', compact('categorys', 'books'));
     }
 
     public function ShowProductDetail($bookid){
@@ -28,18 +27,25 @@ class ProductController extends Controller
         $categorys=bookcategory::all();
         return view('product.ShopItem',compact('books','categorys'));
     }
-
-    public function CartPut(Request $request){
+    public function store(Request $request){
         $user = Auth::user();
         $cart=new Cart;
         $cart-> id = $request['id'];
         $cart-> users_id = $user['email'];
-        $cart-> book_name = $request['bookname'];
-        $cart-> price = $request['price'];
         $cart->save();
-        $books=books::all();
-        $categorys=bookcategory::all();
-        return view('product.index',compact('categorys','books'));
-
+        $books = books::all();
+        $categorys = bookcategory::all();
+        return view('product.index', compact('categorys', 'books'));
+    }
+    public function CartShow(){
+        $user = Auth::user();
+        $user_data = cart::all()->where('users_id',$user['email']);
+        $a=array();
+        foreach ($user_data as $user_data)
+        {
+            array_push($a,$user_data->id);
+        }
+        $books=Books::all()->wherein('id',$a);
+        return view('product.shoppingcart')->with('books',$books);
     }
 }
