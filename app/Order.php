6304990f2935c8dbedd218b195db3404eb3644d9
SKILL573 <a9101021249@gmail.com>
2017-12-28 16:51:31 +0800

    修改ORDER MODEL

--- app/Order.php
+++ app/Order.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     protected $table='order';
-
+    protected $fillable=['id','book_id','discount','quantity','principal','member_id'];
     public function Books(){
         return $this->hasMany(Boosk::class);
     }
